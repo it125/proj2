3d161baa4af701d9b857156673ad1e56676b0a24
it125 <challenge125@daum.net>
2022-02-14 00:19:52 +0900

    20220214

--- js/main.js
+++ js/main.js
@@ -1,95 +1,158 @@
-$('#program .plus').on({
-    "mouseenter": function () {
-        $(this).css({ 'animation': 'rotate1 0.5s ease-in-out' });
-    },
-    "mouseout": function () {
-        $(this).css({ 'animation': 'none' }).css({ 'animation': 'rotate2 0.5s ease-in-out' });
-    }
+// $('#program .plus').on({     "mouseover": function () {         $(this).css({
+// 'animation': 'rotate1 0.5s ease-in-out' });     },     "mouseout": function
+// () {         $(this).css({ 'animation': 'none' }).css({ 'animation': 'rotate2
+// 0.5s ease-in-out' });     } });
+
+$('#program .plus').mouseover(function () {
+    $(this).css({'animation': 'rotate1 0.5s ease-in-out'});
+});
+
+$('#program .plus').mouseout(function () {
+    $(this).css({'animation': 'rotate2 0.5s ease-in-out'});
 });
 
 $('#program .more').on({
     "mouseover": function () {
-        $(this).css({ 'background': '#000' }).css({ 'color': '#fff' }).css({ 'transition': 'all 0.5s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'background': '#000'})
+            .css({'color': '#fff'})
+            .css({'transition': 'all 0.5s ease-in-out'})
+            .css({'cursor': 'pointer'});
         $('#program .more figure img').attr("src", "./images/more_white.png")
     },
     "mouseout": function () {
-        $(this).css({ 'background': 'none' }).css({ 'color': '#000' });
+        $(this)
+            .css({'background': 'none'})
+            .css({'color': '#000'});
         $('#program .more figure img').attr("src", "./images/more_black.png")
     }
 });
 
 $('#academy .more').on({
     "mouseover": function () {
-        $(this).css({ 'background': '#fff' }).css({ 'transition': 'all 0.5s ease-in-out' }).css({ 'cursor': 'pointer' });
-        $('#academy .more > span').css({ 'color': '#000' });
+        $(this)
+            .css({'background': '#fff'})
+            .css({'transition': 'all 0.5s ease-in-out'})
+            .css({'cursor': 'pointer'});
+        $('#academy .more > span').css({'color': '#000'});
         $('#academy .more figure img').attr("src", "./images/more_black.png")
     },
     "mouseout": function () {
-        $(this).css({ 'background': 'none' });
-        $('#academy .more > span').css({ 'color': '#fff' });
+        $(this).css({'background': 'none'});
+        $('#academy .more > span').css({'color': '#fff'});
         $('#academy .more figure img').attr("src", "./images/more_white.png")
     }
 });
 
 $('.daily-content').on({
     "mouseover": function () {
-        $(this).css({ 'flex': '7' }).css({ 'transition': 'all 1s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'flex': '7'})
+            .css({'transition': 'all 1s ease-in-out'})
+            .css({'cursor': 'pointer'});
     },
     "mouseout": function () {
-        $(this).css({ 'flex': '1' }).css({ 'transition': 'all 1s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'flex': '1'})
+            .css({'transition': 'all 1s ease-in-out'})
+            .css({'cursor': 'pointer'});
     }
 });
 
 $('.news-content:nth-child(1) p').on({
     "mouseover": function () {
-        $(this).css({ 'color': '#999' }).css({ 'transition': 'all 0.3s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'color': '#999'})
+            .css({'transition': 'all 0.3s ease-in-out'})
+            .css({'cursor': 'pointer'});
     },
     "mouseout": function () {
-        $(this).css({ 'color': '#000' }).css({ 'cursor': 'auto' });
+        $(this)
+            .css({'color': '#000'})
+            .css({'cursor': 'auto'});
     }
 });
 
 $('.news-content:nth-child(2) p').on({
     "mouseover": function () {
-        $(this).css({ 'color': '#999' }).css({ 'transition': 'all 0.3s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'color': '#999'})
+            .css({'transition': 'all 0.3s ease-in-out'})
+            .css({'cursor': 'pointer'});
     },
     "mouseout": function () {
-        $(this).css({ 'color': '#000' }).css({ 'cursor': 'auto' });
+        $(this)
+            .css({'color': '#000'})
+            .css({'cursor': 'auto'});
     }
 });
 
 $('.news-content:nth-child(3) p').on({
     "mouseover": function () {
-        $(this).css({ 'color': '#999' }).css({ 'transition': 'all 0.3s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'color': '#999'})
+            .css({'transition': 'all 0.3s ease-in-out'})
+            .css({'cursor': 'pointer'});
     },
     "mouseout": function () {
-        $(this).css({ 'color': '#000' }).css({ 'cursor': 'auto' });
+        $(this)
+            .css({'color': '#000'})
+            .css({'cursor': 'auto'});
     }
 });
 
 $('.news-content:nth-child(1) figure').on({
     "mouseover": function () {
-        $(this).css({ 'animation': 'rotate1 0.5s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'animation': 'rotate1 0.5s ease-in-out'})
+            .css({'cursor': 'pointer'});
     },
     "mouseout": function () {
-        $(this).css({ 'animation': 'rotate2 0.5s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'animation': 'rotate2 0.5s ease-in-out'})
+            .css({'cursor': 'pointer'});
     }
 });
 
 $('.news-content:nth-child(2) figure').on({
     "mouseover": function () {
-        $(this).css({ 'animation': 'rotate1 0.5s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'animation': 'rotate1 0.5s ease-in-out'})
+            .css({'cursor': 'pointer'});
     },
     "mouseout": function () {
-        $(this).css({ 'animation': 'rotate2 0.5s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'animation': 'rotate2 0.5s ease-in-out'})
+            .css({'cursor': 'pointer'});
     }
 });
 
 $('.news-content:nth-child(3) figure').on({
     "mouseover": function () {
-        $(this).css({ 'animation': 'rotate1 0.5s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'animation': 'rotate1 0.5s ease-in-out'})
+            .css({'cursor': 'pointer'});
     },
     "mouseout": function () {
-        $(this).css({ 'animation': 'rotate2 0.5s ease-in-out' }).css({ 'cursor': 'pointer' });
+        $(this)
+            .css({'animation': 'rotate2 0.5s ease-in-out'})
+            .css({'cursor': 'pointer'});
     }
 });
+
+// var selectTab = document.getElementById("academyTab"); 
+// var con = document.getElementsByClassName("academy-content");
+// selectTab.addEventListener("change", function () {
+//     var val = selectTab.options[selectTab.selectedIndex].value;
+//     for (var i = 0; i < selectTab.length; i++) { 
+//         con[i].style.display = "none";
+//         if (val == "academy1") {
+//             con[i].style.display = "block";
+//             documnet.getElementById("select1").selected = true;
+//         } else if (val == "academy2") {
+//             con[1].style.display = "block";
+//             documnet.getElementById("select2").selected = true;
+//             $('.academy-content').css({'background': 'url(../images/academy2_bg_dark.png)'}); 
+//         }
+//     }
+// });
